ee2ff6184baa691c22c815f9e856d368d3fe7aa5
Keki Burjorjee <kburjorjee@pandora.com>
2015-08-10 00:07:42 -0700

    Add password generation from a password specification file. Add multiprocessing

--- pyethrecover.py
+++ pyethrecover.py
@@ -10,6 +10,10 @@ import binascii
 import pbkdf2 as PBKDF2
 from bitcoin import *
 from utils import decode_hex, encode_hex
+import traceback
+from joblib import Parallel, delayed
+import itertools
+
 try:
     from urllib2 import Request, urlopen
 except ImportError:
@@ -30,8 +34,11 @@ parser.add_option('-p', '--password',
                   default=None, dest='pw',
                   help="A single password to try against the wallet.")
 parser.add_option('-f', '--passwords-file',
-                  default="pwds.txt", dest='pwfile',
+                  default=None, dest='pwfile',
                   help="A file containing a newline-delimited list of passwords to try. (default: %default)")
+parser.add_option('-s', '--password-spec-file',
+                  default=None, dest='pwsfile',
+                  help="A file containing a password specification")
 parser.add_option('-w', '--wallet',
                   default='wallet.json', dest='wallet',
                   help="The wallet against which to try the passwords. (default: %default)")
@@ -118,14 +125,23 @@ def eth_privtoaddr(priv):
     return encode_hex(sha3(pub)[12:])
 
 
+class DecryptionException(Exception):
+    pass
+
 def getseed(encseed, pw, ethaddr):
     try:
         seed = aes.decryptData(pw, binascii.unhexlify(encseed))
+    except Exception, e:
+        raise DecryptionException("AES Decryption error. Bad password?")
+    try:
         ethpriv = sha3(seed)
         eth_privtoaddr(ethpriv)
         assert eth_privtoaddr(ethpriv) == ethaddr
-    except:
-        raise Exception("Decryption failed. Bad password?")
+    except Exception, e:
+        # print ("eth_priv = %s" % eth_privtoaddr(ethpriv))
+        # print ("ethadd = %s" % ethaddr)
+        # traceback.print_exc()
+        raise DecryptionException("Decryption failed. Bad password?")
     return seed
 
 
@@ -140,6 +156,39 @@ def ask_for_password():
     return getpass.getpass()
 
 
+class PasswordFoundException(Exception):
+    pass
+
+def crack(wallet_filename, grammar):
+    with file(wallet_filename, 'r') as f:
+        t = f.read()
+    w = json.loads(t)
+    try:
+        Parallel(n_jobs=-1)(delayed(attempt)(w, pw) for pw in generate_all(grammar,''))
+    except Exception, e:
+        traceback.print_exc()
+        while True:
+            sys.stdout.write('\a')
+            sys.stdout.flush()
+
+def generate_all(el, tr):
+    if el:
+        for j in xrange(len(el[0])):
+            for w in generate_all(el[1:], tr + el[0][j]):
+                yield w
+    else:
+        yield tr
+
+def attempt(w, pw):
+    try:
+        print (pw)
+        raise PasswordFoundException(
+            """\n\nYour seed is:\n%s\nYour password is:\n%s""" % (getseed(w['encseed'], pbkdf2(pw), w['ethaddr']), pw))
+
+    except DecryptionException as e:
+        # print(e)
+        return ""
+
 def __main__():
     w = tryopen(options.wallet)
     if not w:
@@ -147,6 +196,9 @@ def __main__():
         exit(1)
 
     pwds = []
+
+    if not(options.pw or options.pwfile or options.pwsfile):
+        print("No passwords specified! (-h for help)")
 
     if options.pw:
         pwds.append(options.pw)
@@ -158,18 +210,18 @@ def __main__():
             print("Password file not found! (-h for help)")
             exit(1)
 
-    if len(pwds) == 0:
-        pwds.append(ask_for_password())
+    if options.pwsfile:
+        grammar = eval(file(options.pwsfile, 'r').read())
+        pwds = itertools.chain(pwds, generate_all(grammar,''))
 
-    for pw in pwds:
-        try:
-            print("\n\nYour seed is:\n%s" % getseed(w['encseed'], pbkdf2(pw), w['ethaddr']))
-            print("\nYour password is:\n%s\n" % pw);
-            exit(0)
-        except Exception as e:
-            if not options.pwfile:
-                raise
-            print("x", end="")
+
+    try:
+        Parallel(n_jobs=-1)(delayed(attempt)(w, pw) for pw in pwds)
+    except Exception, e:
+        traceback.print_exc()
+        while True:
+            sys.stdout.write('\a')
+            sys.stdout.flush()
 
 if __name__ == "__main__":
     __main__()
